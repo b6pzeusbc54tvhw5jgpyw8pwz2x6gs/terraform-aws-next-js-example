7b463b57e787332064a4ef625865c3cb5fe1a402
aluc <b6pzeusbc54tvhw5jgpyw8pwz2x6gs@gmail.com>
2021-01-24 21:17:56 +0900

    dx: add infra directory

---
+++ infra/backend.hcl
@@ -0,0 +1,6 @@
+region         = "ap-northeast-2"
+bucket         = "tf-next-tfstate-b6b272d84035"
+key            = "tf-next.tfstate"
+encrypt        = true
+kms_key_id     = "arn:aws:kms:ap-northeast-2:539425821792:key/e18d2281-3cb7-4c84-b82c-31f9916dd9fc"
+dynamodb_table = "tf-next-tfstate-lock"
---
+++ infra/main.tf
@@ -0,0 +1,24 @@
+terraform {
+  required_version = "~> 0.14.5"
+  backend "s3" {}
+}
+
+provider "aws" {
+  region = "eu-west-1"
+}
+
+provider "aws" {
+  alias = "virginia"
+  region = "us-east-1"
+}
+
+module "tf-next" {
+  // source = "github.com/b6pzeusbc54tvhw5jgpyw8pwz2x6gs/terraform-aws-next-js"
+  // for local test
+  source = "../../terraform-aws-next-js"
+  next_tf_dir = "../.n"
+}
+
+output "domain" {
+  value = module.tf-next.cloudfront_domain_name
+}
